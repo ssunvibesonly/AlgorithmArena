a20ff73a2c23f5fe20f37df8bc82571653c0d636
osumin <osn9274@naver.com>
2024-11-11 22:09:26 +0900

    테스트 커밋입니다.

--- sumin/Test1.java
+++ sumin/Test1.java
@@ -2,6 +2,6 @@ package AlgorithmArena.sumin;
 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test2");
     }
 }
